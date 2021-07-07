29fa3258ab78c5806183eeaf40b904c5bc134c24
tomasbarak <barakcinthia@gmail.com>
2021-07-07 19:19:06 -0300

    Added top-nav to alerts page

--- public/scripts/alerts/get-alerts.js
+++ public/scripts/alerts/get-alerts.js
@@ -17,9 +17,10 @@ function add_markers(map, alertItems){
     for(let key in alertItems){
         var marker = L.marker([alertItems[key].Coordinates.lat, alertItems[key].Coordinates.lon]).addTo(map);
         marker.bindPopup("<div class='alertCont'><img src='' alt='logo' class='alertImage' id='alertImage'><a id='alertName' style='color: black; font-size: 8pt'>alerts.Name</a></div>").openPopup();
-        document.getElementById("alertImage").id = alertItems[key].Id;
-        document.getElementById(alertItems[key].Id).src = alertItems[key].Photo;
-        document.getElementById("alertName").innerText = alertItems[key].Name;
+        document.getElementById("alertImage").id = alertItems[key].Id + "Image";
+        document.getElementById("alertName").id = alertItems[key].Id + "Name";
+        document.getElementById(alertItems[key].Id + "Image").src = alertItems[key].Photo;
+        document.getElementById(alertItems[key].Id + "Name").innerText = alertItems[key].Name;
         console.log(alertItems);
     }
 }
