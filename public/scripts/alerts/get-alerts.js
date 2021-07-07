cc3372773956eeda056d2347887e050b772f6bb0
tomasbarak <barakcinthia@gmail.com>
2021-07-07 18:46:32 -0300

    Added new features

--- public/scripts/alerts/get-alerts.js
+++ public/scripts/alerts/get-alerts.js
@@ -17,7 +17,8 @@ function add_markers(map, alertItems){
     for(let key in alertItems){
         var marker = L.marker([alertItems[key].Coordinates.lat, alertItems[key].Coordinates.lon]).addTo(map);
         marker.bindPopup("<div class='alertCont'><img src='' alt='logo' class='alertImage' id='alertImage'><a id='alertName' style='color: black; font-size: 8pt'>alerts.Name</a></div>").openPopup();
-        document.getElementById("alertImage").src = alertItems[key].Photo;
+        document.getElementById("alertImage").id = alertItems[key].Id;
+        document.getElementById(alertItems[key].Id).src = alertItems[key].Photo;
         document.getElementById("alertName").innerText = alertItems[key].Name;
         console.log(alertItems);
     }
