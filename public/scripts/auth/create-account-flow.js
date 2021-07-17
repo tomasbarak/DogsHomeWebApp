b3d8c4bd9dc866ddbf052b8d57c2d51c6d40c5f4
tomasbarak <barakcinthia@gmail.com>
2021-07-16 22:16:06 -0300

    Added instances for sign up

---
+++ public/scripts/auth/create-account-flow.js
@@ -0,0 +1,44 @@
+var actualInstance;
+
+function setInicialInstance(){
+    actualInstance = 0;
+}
+
+function submitInstance(){
+    console.log(actualInstance);
+    switch (actualInstance){
+        //email y contraseña
+        case 0:
+            break;
+        //Nombre y apellido
+        case 2:
+            break;
+        //Tipo de cuenta
+        case 3:
+            break;
+        //Nombre del refugio
+        case 4:
+            break;
+        //Foto de perfil
+        case 5:
+            break;
+        //Telefono(s)
+        case 6:
+            break;
+        //Descripcion corta
+        case 7:
+            break;
+        //Descripcion larga
+        case 8:
+            break;
+        //Sitio web
+        case 9:
+            break;
+        //Redes sociales
+        case 9:
+            break;
+        //Terminos y condiciones
+        case 10:
+            break;
+    }
+}
--- public/scripts/auth/sign-up.js
+++ public/scripts/auth/sign-up.js
@@ -1,3 +1,23 @@
+function enter(email, password, repeatPassword){
+    if(event.key === 'Enter') {
+        if(password === repeatPassword){
+            signUp(email, password);
+        }else{
+
+        }
+    }
+}
+
+function throwEmailExistsError(){
+    Swal.fire({
+            title: 'Error',
+            text: 'Parece que el email ingresado ya existe',
+            icon: 'error',
+            confirmButtonColor: '#d33',
+    }
+    )
+}
+
 function signUp(email, password){
     firebase.auth().createUserWithEmailAndPassword(email, password)
         .then((userCredential) => {
@@ -12,6 +32,12 @@ function signUp(email, password){
         .catch((error) => {
             var errorCode = error.code;
             var errorMessage = error.message;
-            // ..
+            if(errorCode === 'auth/email-already-in-use'){
+                throwEmailExistsError();
+            }
         });
+}
+
+function confirmIfMailExists(email){
+
 }
