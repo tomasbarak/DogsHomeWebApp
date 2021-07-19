b296ca20e6e5c687600232296c940bc0a0a93c11
tomasbarak <barakcinthia@gmail.com>
2021-07-19 02:00:42 -0300

    Added new instance

--- public/scripts/auth/auth-flow-autorefresh.js
+++ public/scripts/auth/auth-flow-autorefresh.js
@@ -1,6 +1,8 @@
-function refreshInstances(){
-    switch (actualInstance){
+function refreshInstances(instance){
+    //console.log(instance)
+    switch(instance){
         case undefined:
+
             setInstanceVisibility('email-and-password');
             break;
         case 0:
@@ -9,10 +11,13 @@ function refreshInstances(){
         //Nombre y apellido
         case 1:
             setInstanceVisibility('name-and-surname');
+            setInstanceLabel('Ingresá tu nombre');
             break;
         //Tipo de cuenta
         case 2:
+            console.log('instance 2')
             setInstanceVisibility('account-type');
+            setInstanceLabel('Elegí el tipo de cuenta');
             break;
         //Nombre del refugio
         case 3:
@@ -39,20 +44,29 @@ function refreshInstances(){
         case 10:
             break;
     }
+    //console.log(Number(instance))
+
 }
 
 function setInstanceVisibility(instanceIdName){
+    console.log(instanceIdName);
+
     var allInstances = document.getElementsByClassName('signup-instance');
     for(let key in allInstances){
         //console.log(allInstances[key].id);
         if(allInstances[key].id !== undefined){
             if(allInstances[key].id === instanceIdName){
-                console.log(allInstances[key]);
+                //console.log(allInstances[key]);
                 allInstances[key].style.display = 'flex';
             }else{
-                console.log(allInstances[key]);
+                //console.log(allInstances[key]);
                 allInstances[key].style.display = 'none';
             }
         }
     }
+}
+function setInstanceLabel(text){
+    var instanceLabel = document.getElementById('instance-label');
+
+    instanceLabel.innerText = text;
 }
--- public/scripts/auth/create-account-flow.js
+++ public/scripts/auth/create-account-flow.js
@@ -1,53 +1,132 @@
 var actualInstance;
+var accTypeSelection;
+
+setBackButtonVisibility();
 
 function setInicialInstance(inicialNumber){
     if(inicialNumber !== null || inicialNumber !== undefined){
-        actualInstance = inicialNumber;
+        //console.log(sessionStorage.getItem('sessionInstance'))
+        if(sessionStorage.getItem('sessionInstance') !== null && sessionStorage.getItem('sessionInstance') !== undefined){
+            actualInstance = Number(sessionStorage.getItem('sessionInstance'));
+            //console.log(actualInstance)
+        }else{
+            //console.log("nashenashe")
+            actualInstance = inicialNumber;
+            sessionStorage.setItem('sessionInstance', inicialNumber);
+        }
     }else{
+        //console.log("nashenashe2")
         actualInstance = 0;
+        sessionStorage.setItem('sessionInstance', '0');
     }
-    refreshInstances();
+    //console.log(actualInstance)
+    setBackButtonVisibility(Number(actualInstance))
+    refreshInstances(Number(actualInstance));
+
+
+}
+function setBackButtonVisibility(instance){
+    if(instance <= 1 || instance === undefined || instance === null){
+        document.getElementById('back-button').style.display = 'none';
+        console.log('nashe1');
+        console.log( actualInstance );
+
+    }else{
+        document.getElementById('back-button').style.display = 'flex';
+        console.log('nashe2');
+        console.log( actualInstance );
+    }
+}
+function submitInstance(){
+    if(actualInstance < 2){
+        console.log(actualInstance);
+        switch (actualInstance){
+            //email y contraseña
+            case 0:
+                signUp(document.getElementById('mail').value, document.getElementById('password').value, document.getElementById('password-repeat').value);
+                break;
+            //Nombre y apellido
+            case 1:
+                saveNameAndSurnameValues();
+                break;
+            //Tipo de cuenta
+            case 2:
+                saveAccountType(accTypeSelection);
+                setAccTypeSelection(accTypeSelection);
+                break;
+            //Nombre del refugio
+            case 3:
+                break;
+            //Foto de perfil
+            case 4:
+                break;
+            //Telefono(s)
+            case 5:
+                break;
+            //Descripcion corta
+            case 6:
+                break;
+            //Descripcion larga
+            case 7:
+                break;
+            //Sitio web
+            case 8:
+                break;
+            //Redes sociales
+            case 9:
+                break;
+            //Terminos y condiciones
+            case 10:
+                break;
+        }
+        actualInstance ++;
+        setBackButtonVisibility(actualInstance);
+        sessionStorage.setItem('sessionInstance', actualInstance);
+        refreshInstances(actualInstance);
+    }
 
 }
 
-function submitInstance(){
-    console.log(actualInstance);
-    switch (actualInstance){
-        //email y contraseña
-        case 0:
-            signUp(document.getElementById('mail').value, document.getElementById('password').value, document.getElementById('password-repeat').value);
-            break;
-        //Nombre y apellido
+function instanceGoBack(){
+    if(actualInstance > 1){
+        actualInstance --;
+        setBackButtonVisibility();
+        sessionStorage.setItem('sessionInstance', actualInstance);
+        refreshInstances(actualInstance);
+    }
+}
+
+function  setAccTypeSelection(selection){
+    var accountTypesContainer = document.getElementById('account-types-container');
+
+    var accountTypes = accountTypesContainer.getElementsByClassName('account-type-option-container');
+
+    switch (selection){
         case 1:
+            document.getElementById('adoptante').className = 'account-type-option acc-type-selected';
+            document.getElementById('refugio').className = 'account-type-option acc-type-unselected';
+            document.getElementById('rescatista-solo').className = 'account-type-option acc-type-unselected';
             break;
-        //Tipo de cuenta
         case 2:
+            document.getElementById('adoptante').className = 'account-type-option acc-type-unselected';
+            document.getElementById('refugio').className = 'account-type-option acc-type-selected';
+            document.getElementById('rescatista-solo').className = 'account-type-option acc-type-unselected';
             break;
-        //Nombre del refugio
         case 3:
-            break;
-        //Foto de perfil
-        case 4:
-            break;
-        //Telefono(s)
-        case 5:
-            break;
-        //Descripcion corta
-        case 6:
-            break;
-        //Descripcion larga
-        case 7:
-            break;
-        //Sitio web
-        case 8:
-            break;
-        //Redes sociales
-        case 9:
-            break;
-        //Terminos y condiciones
-        case 10:
+            document.getElementById('adoptante').className = 'account-type-option acc-type-unselected';
+            document.getElementById('refugio').className = 'account-type-option acc-type-unselected';
+            document.getElementById('rescatista-solo').className = 'account-type-option acc-type-selected';
             break;
     }
-    actualInstance ++;
-    refreshInstances();
+}
+
+function saveNameAndSurnameValues(){
+    var nameInputValue = document.getElementById('name').value;
+    var surnameInputValue = document.getElementById('surname').value;
+
+    sessionStorage.setItem('sessionAccName', nameInputValue);
+    sessionStorage.setItem('sessionAccSurname', surnameInputValue);
+}
+function saveAccountType(selection){
+    sessionStorage.setItem('sessionAccType', selection);
 }
